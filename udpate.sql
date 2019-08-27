15200b3fb6e0caa8aed49b88542baaab60676496
draoco <dejie20@126.com>
2019-08-27 23:35:40 +0800

    update

--- udpate.sql
+++ udpate.sql
@@ -1,1 +1,6 @@
 UPDATE `test`.`remind_word` SET `tag` = 'mini', `content` = '表示“小”', `connect` = '迷你' WHERE `id` = 48;
+UPDATE `test`.`remind_word` SET `tag` = 'mono', `content` = '表示“单个，一个”', `connect` = '一个白发魔女,头发只有一根白色' WHERE `id` = 49;
+UPDATE `test`.`remind_word` SET `tag` = 'Multi', `content` = '表示”很多,很多”', `connect` = '木楼梯的阶梯很多很多' WHERE `id` = 50;
+UPDATE `test`.`remind_word` SET `tag` = 'neo', `content` = '表示”新的”', `connect` = '买了一个新的梳子,上面有no字, 说不需要自己动手' WHERE `id` = 51;
+UPDATE `test`.`remind_word` SET `tag` = 'non', `content` = '表示”不,非”', `connect` = '不要杀我,我举双手' WHERE `id` = 52;
+UPDATE `test`.`remind_word` SET `tag` = 'omni', `content` = '表示”全部  到处”', `connect` = '前面一个人, 后面是你,到处都是这样的情景' WHERE `id` = 53;
